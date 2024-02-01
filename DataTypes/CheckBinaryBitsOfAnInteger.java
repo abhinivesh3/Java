a83723a26d6498f4247845727ce70e78424af920
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-01 21:28:06 +0530

    DataTypes-CheckBinaryBitsOfAnInteger

---
+++ DataTypes/CheckBinaryBitsOfAnInteger.java
@@ -0,0 +1,11 @@
+package DataTypes;
+
+public class CheckBinaryBitsOfAnInteger {
+    public static void main(String arg[]){
+        int x= 5;
+        //int x =-5 ;//It will convert into 2nd complementation
+        System.out.println("Integer to Binary : "+Integer.toBinaryString(x));
+        System.out.println("Integer to Binary : "+Integer.toBinaryString(x));
+        System.out.println("Integer to Binary : "+Integer.toBinaryString(x));
+    }
+}
