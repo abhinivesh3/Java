17b426a7848fa18a50e77980db3184ed2758df86
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-01 17:09:07 +0530

    DataSizeRange-Variables

---
+++ DataTypes/Variables.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public abstract class Variables {
+    public static void main(String arg[]){
+        byte b = 127;
+        short s = 300;
+        int i = 100;
+        float f = 25.3f;
+        char c = 'A';
+        System.out.println("byte : "+b);
+        System.out.println("short : "+s);
+        System.out.println("int : "+i);
+        System.out.println("float : "+f);
+        System.out.println("char : "+c);
+    }
+}
