524da8bc2efa704856cdaa65a9c67229d1ea4f11
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-02 00:47:25 +0530

    OperatorAndExpression-ArithmetricOperator

---
+++ OperatorAndExpressions/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+package OperatorAndExpressions;
+
+public class ArithmeticOperators {
+    public static void main(String[] args){
+        int a=14,b=5;
+        float c=(float)a/b;
+        System.out.println("Addition : "+(a+b));
+        System.out.println("Substraction : "+(a-b));
+        System.out.println("Multiplication : "+(a*b));
+        System.out.println("Division : "+c);
+        System.out.println("Modulo : "+(a%b));
+        System.out.println((10+20)/2);
+    }
+}
