8c033c5cec8ba88c00234362aae929afd94b4228
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-01 16:23:54 +0530

    First program

---
+++ Introduction/src/First.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I know Your Name");
+        String name = scanner.nextLine();
+        System.out.println("Welcome " + name);
+    }
+}
