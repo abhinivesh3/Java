121faf308c19be82c1e58b37f050531abbf928ac
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-01 16:56:19 +0530

    DataTypes-DataSizeRanges\

---
+++ DataTypes/DataSizeRange.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class DataSizeRange {
+    public static void main(String arg[]){
+        System.out.println("Int Min: "+Integer.MIN_VALUE);
+        System.out.println("Int Max: "+Integer.MAX_VALUE);
+        System.out.println("Int Bytes : "+Integer.BYTES);//int size
+    
+        System.out.println("Byte Min: "+Byte.MIN_VALUE);
+        System.out.println("Byte Max: "+Byte.MAX_VALUE);
+        System.out.println("Byte Bytes : "+Byte.BYTES);
+   
+    }
+}
