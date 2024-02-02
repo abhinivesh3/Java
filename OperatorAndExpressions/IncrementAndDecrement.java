aec636ac7c5d01fbbc783c39cc7541cebfae41d4
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-02 16:12:56 +0530

    OperatorAndExpressions-IncrementAndDecrement

---
+++ OperatorAndExpressions/IncrementAndDecrement.java
@@ -0,0 +1,11 @@
+package OperatorAndExpressions;
+
+public class IncrementAndDecrement {
+    public static void main(String args[]){
+        int x=5;
+        System.out.println("Post Increament : "+(x++));
+        System.out.println("Pre Increment : "+(++x));
+        System.out.println("Post Decrement : "+(x--));
+        System.out.println("Pre Decrement : "+(--x));
+    }
+}
