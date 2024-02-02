930d655ec977128b5df31f13b5ad3d1e1c2f240f
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-02 19:59:45 +0530

    StringClassAndPrinting-Printing

---
+++ StringClassAndPrinting/Printing.java
@@ -0,0 +1,11 @@
+package StringClassAndPrinting;
+
+public class Printing {
+    public static void main(String args[]){
+        int a=7;
+        System.out.print(a);
+        System.out.println("value"+a);
+        System.out.printf("value\n");
+        System.out.printf("value %d\n", a);
+    }
+}
