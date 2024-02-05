b83f5b5d40a9787d513a2d7995eb0583c88771b6
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-05 20:05:31 +0530

    Abstraction-program

---
+++ OOPs/Abstraction/AbstractRules.java
@@ -0,0 +1,19 @@
+package OOPs.Abstraction;
+
+abstract class Super{
+    abstract public void meth1();
+}
+class Sub extends Super{
+    public void meth1(){
+        System.out.println("Abstract");
+    }
+}
+
+public class AbstractRules {
+    public static void main(String[] args) {    
+        Super s;   
+        s=new Sub();
+        s.meth1();
+    }
+    
+}
