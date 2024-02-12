1d0ec5b0753581ffd65a932a27f6adb91b724f85
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-02-12 14:55:43 +0530

    StaticAndFinal and Singleton class

---
+++ StaticAndFinal/FinalMember.java
@@ -0,0 +1,35 @@
+package StaticAndFinal;
+
+class Test {
+    public final void show(){
+        System.out.println();
+    }
+}
+
+class Test1 extends Test{
+    //public void show(){}
+    //gives error as final methods cannot be overloaded.
+}
+
+public class FinalMember {
+
+    /*final float PI;
+    
+    public FinalDemo(){
+        PI=3.1425f;
+    }*/
+    /*static final float PI;
+    
+    static{
+        PI=3.1425f;
+    }*/
+    
+    public static void main(String[] args) {
+        
+        /*final float PI;
+        PI=3.1425f;
+        System.out.println(PI);*/
+        
+    }
+    
+}
