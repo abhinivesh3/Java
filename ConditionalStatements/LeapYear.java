c89c089969c5682eb6ab679b36102425c265d6db
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2024-03-03 01:23:22 +0530

    Condition changed for LeapYear

--- ConditionalStatements/LeapYear.java
+++ ConditionalStatements/LeapYear.java
@@ -7,14 +7,14 @@ public class LeapYear {
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter a Year :");
         int year=scanner.nextInt();
-        if(year%4==0 || year%100==0 && year%400==0){
+        if(year%400==0 || year%100!=0 && year%4==0){
             System.out.println("Leap Year");
         }
         else{
             System.out.println("Not Leap Year");
         }
         if(year%4==0){
-            if(year%100==0){
+            if(year%100!=0){
                 if(year%400==0){
                     System.out.println("Leap year");
                 }
